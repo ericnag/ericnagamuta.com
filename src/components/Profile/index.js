1a507cf870f1ce363a95aef94ae69f911eddb928
Eric Nagamuta <enagamuta@gmail.com>
2020-01-30 22:42:37 -0400

    Created a Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Eric Nagamuta</h1>
+    <h2>Full Stack Developer</h2>
+    <p>
+      Desenvolvedor Full Stack que preza por explorar novas tecnologias e
+      encarar grandes desafios. Familiarizado com Java, porém entusiasta pela
+      Linguagem Javascript e seus Frameworks como ReactNative, ReactJS e NodeJS.
+      Também possui conhecimento com Base de dados (Oracle e Mongodb), Cloud
+      (AWS), GIT, Kafka, Linux, entre outras tecnologias vinculadas.
+    </p>
+  </div>
+)
+
+export default Profile
